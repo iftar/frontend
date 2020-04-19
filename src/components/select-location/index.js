361f6bbaa8dc3cf4492f26970efa4e800093b7c2
BravoTwoZero <sofiul.alam@hotmail.com>
2020-04-19 17:09:53 +0100

    Add Location API call with Token

--- src/components/select-location/index.js
+++ src/components/select-location/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import  Location  from './location';
+import React from "react";
+import Location from "./location";
 
 const SelectionLocation = () => {
   return (
-    <div>
-      Selection Location page
-    </div>,
-    <div>
-      <Location />
-    </div>
-  )
-}
+    (<div>Selection Location page</div>),
+    (
+      <div>
+        <Location />
+      </div>
+    )
+  );
+};
 
-export default SelectionLocation
+export default SelectionLocation;
--- src/util/api.js
+++ src/util/api.js
@@ -1,38 +1,40 @@
-import axios from 'axios'
-import { saveItem } from './storage'
+import axios from "axios";
+import { saveItem, getItem } from "./storage";
 
-const BASE_URL = 'https://share-your-iftar-backend.herokuapp.com/api'
+const BASE_URL = "https://share-your-iftar-backend.herokuapp.com/api";
+const LOCATION_URL =
+  "https://share-your-iftar-backend.herokuapp.com/api/collection-points";
+const ORDER_CHECK_URL =
+  "https://share-your-iftar-backend.herokuapp.com//api/user/orders/check";
 
 export async function login(email, password) {
-  let response
+  let response;
 
   try {
     response = await axios.post(`${BASE_URL}/login`, {
       email,
       password,
-    })
-    const data = response.data
+    });
+    const data = response.data;
 
     if (response.status === 200) {
-      const user = data.data.user
-      const token = data.data.token
+      const user = data.data.user;
+      const token = data.data.token;
 
       // Storing user info and token in localStorage
-      saveItem("userToken", token)
-      saveItem("user", JSON.stringify(user))
+      saveItem("userToken", token);
+      saveItem("user", JSON.stringify(user));
     }
 
-    return data
-  }
-  catch (error) {
-    console.log('error: ', error.response)
-    return error.response.data
+    return data;
+  } catch (error) {
+    console.log("error: ", error.response);
+    return error.response.data;
   }
 }
 
-
 export async function register(firstname, lastname, email, password, confirm) {
-  let response
+  let response;
 
   try {
     response = await axios.post(`${BASE_URL}/register`, {
@@ -40,18 +42,50 @@ export async function register(firstname, lastname, email, password, confirm) {
       lastname,
       email,
       password,
-      confirm
-    })
+      confirm,
+    });
 
-    const data = response.data
+    const data = response.data;
 
-    console.log('data: ', data)
+    console.log("data: ", data);
 
-    return data
-  }
-  catch (error) {
-    console.log('error: ', error.response)
-    return error.response.data
+    return data;
+  } catch (error) {
+    console.log("error: ", error.response);
+    return error.response.data;
   }
 }
 
+export async function ordercheck(userId, user_can_order) {
+  let response;
+
+  try {
+    response = await axios.get(`${ORDER_CHECK_URL}?id={userId}`, {
+      user_can_order,
+    });
+    const data = response.data;
+    console.log("data: ", data);
+    return data;
+  } catch (error) {
+    console.log("error: ", error.response);
+    return error.response.data;
+  }
+}
+
+export async function getLocations() {
+  const token = getItem("userToken");
+  let response;
+  try {
+    response = await axios.get(`${LOCATION_URL}`, {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+    const data = response.data;
+    console.log("data: ", data);
+    return Object.assign(new Location(), data);
+  } catch (error) {
+    console.log("error: ", error.response);
+    return error.response.data;
+  }
+}
