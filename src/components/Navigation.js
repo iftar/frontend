71acdeabbfad847356e3247252bb71e8c7ce0016
mafz <mafzdeveloper@gmail.com>
2020-04-20 17:38:22 +0100

    fix navigation closing on click

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,16 @@
 import View from './element-wrappers/View';
 import {Button, Image, Nav, Navbar, NavDropdown} from 'react-bootstrap';
 import logo from '../assets/images/shareiftar-logo.png';
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {logout} from '../store/auth/actions';
+import {Link} from 'react-router-dom';
+import {
+  URL_CREATE_ORDER, URL_LOGIN,
+  URL_ORDERS,
+  URL_SELECT_LOCATION, URL_SIGN_UP,
+} from '../constants/urls';
 
 type Props = {
   user: User,
@@ -14,8 +20,18 @@ type Props = {
 
 function Navigation(props : Props) {
 
+  const [expanded, setExpanded] = useState(false);
+
   function logout() {
     props.logout();
+  }
+
+  function onSelect() {
+    setExpanded(false);
+  }
+
+  function onToggle() {
+    setExpanded(!expanded)
   }
 
   return (
@@ -26,25 +42,25 @@ function Navigation(props : Props) {
         alignItems: 'center',
         justifyContent: 'center',
       }}>
-        <Navbar expand={false} style={{width: "100%"}}>
+        <Navbar expand={false} expanded={expanded} style={{width: "100%"}} onToggle={onToggle}>
           <Navbar.Brand>
             <Image src={logo} height={"60px"} />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Toggle onClick={onToggle}/>
           <Navbar.Collapse id="">
             <Nav className="mr-auto">
               {props.user != null ?
                   <Fragment>
-                    <Nav.Link href="/select-location">Select Location</Nav.Link>
-                    <Nav.Link href="/create-order">Create Order</Nav.Link>
-                    <Nav.Link href="/orders">My orders</Nav.Link>
+                    <Link to={URL_SELECT_LOCATION} onClick={onSelect}>Select Location</Link>
+                    <Link to={URL_CREATE_ORDER} onClick={onSelect}>Create Order</Link>
+                    <Link to={URL_ORDERS} onClick={onSelect}>My orders</Link>
                     <NavDropdown.Divider />
                     <Button variant={"info"} block onClick={logout}>Logout</Button>
                   </Fragment>
                   :
                   <Fragment>
-                    <Nav.Link  href="/login">Login</Nav.Link>
-                    <Nav.Link href="/sign-up">Sign up</Nav.Link>
+                    <Link to={URL_LOGIN} onClick={onSelect}>Login</Link>
+                    <Link to={URL_SIGN_UP} onClick={onSelect}>Sign up</Link>
                   </Fragment>
               }
             </Nav>
