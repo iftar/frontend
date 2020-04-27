a327ad79aa38d5d3cd06cbb3b714dc2bc56ebdd2
mafz <mafzdeveloper@gmail.com>
2020-04-27 21:46:41 +0100

    hide deliveries/collections option when collection point not accepting deliveries or collections

--- src/views/create-order/CreateOrderView.js
+++ src/views/create-order/CreateOrderView.js
@@ -237,22 +237,26 @@ const CreateOrderView = (props: Props) => {
               width: "100%"
             }}
           >
+            {props.collectionPoint.accepting_collections &&
             <Form.Check
-              name="collection-or-delivery"
-              type={"radio"}
-              label={"Collection"}
-              id={"radio-collection"}
-              onChange={onCollectionSelected}
-              checked={isCollection}
+                name="collection-or-delivery"
+                type={"radio"}
+                label={"Collection"}
+                id={"radio-collection"}
+                onChange={onCollectionSelected}
+                checked={isCollection}
             />
+            }
+            {props.collectionPoint.accepting_deliveries &&
             <Form.Check
-              name="collection-or-delivery"
-              type={"radio"}
-              label={"Delivery"}
-              id={"radio-delivery"}
-              onChange={onDeliverySelected}
-              checked={!isCollection}
+                name="collection-or-delivery"
+                type={"radio"}
+                label={"Delivery"}
+                id={"radio-delivery"}
+                onChange={onDeliverySelected}
+                checked={!isCollection}
             />
+            }
           </View>
           {!isCollection && (
             <View
