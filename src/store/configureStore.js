12f8fc67015722892ccf8b66293e65dac4367991
mafz <mafzdeveloper@gmail.com>
2020-05-04 18:26:45 +0100

    Remove devtool on production build

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -5,7 +5,18 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
 import { createLogger } from "redux-logger/src";
 
-export default createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk, createLogger()))
-);
+let store;
+
+if (process.env.NODE_ENV === 'production') {
+  store = createStore(
+      rootReducer,
+      applyMiddleware(thunk)
+  )
+} else {
+  store = createStore(
+      rootReducer,
+      composeWithDevTools(applyMiddleware(thunk, createLogger()))
+  )
+}
+
+export default store;
